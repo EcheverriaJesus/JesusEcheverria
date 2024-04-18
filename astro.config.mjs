99aa994e694246fe184b0a1441cd152970b3b5b0
noe <jesus.echeverria017@gmail.com>
2024-04-18 11:48:31 -0600

    deploy#13

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind({
+    // Example: Provide a custom path to a Tailwind config file
+    applyBaseStyles: false,
+    configFile: './custom-config.mjs',
+  }),
+  react()],
   site: 'https://echeverriajesus.github.io/JesusEcheverria/',
   base: 'my-repo',
 });
